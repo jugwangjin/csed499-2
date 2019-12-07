77a11825ac8c8b0434fff3bffc5be77399363800
Ju Gwangjin <gwangjin@postech.ac.kr>
2019-12-07 15:37:09 +0900

    transfer

--- radialRegression.py
+++ radialRegression.py
@@ -65,7 +65,7 @@ def make_image_batch(fileNameBatch):
 	images = images/255
 	return images
 
-image_shape = [250, 250, 3]
+image_shape = [224, 224, 3]
 
 fMin = 0.1
 fMax = 1
@@ -106,78 +106,145 @@ def make_image_batch(fileNameBatch):
     images = images/255
     return images
 
-class FRegression(nn.Module):
-    
-    def __init__(self):
-        # 항상 torch.nn.Module을 상속받고 시작
-        super(FRegression, self).__init__()
-        #input : 3*480*480
-        conv1_1 = nn.Conv2d(3, 16, 3, 1, 1) # 16 * 480 * 480
-        conv1_2 = nn.Conv2d(16, 32, 3, 1, 1) # 96 * 160 * 160
-        pool1 = nn.MaxPool2d(3) # 128 * 80 *80
-
-        conv2_1 = nn.Conv2d(32, 64, 3, 1, 1) # 128 * 160 * 160
-        conv2_2 = nn.Conv2d(64, 96, 5, 1, 2) # 160 * 80 * 80
-        pool2 = nn.MaxPool2d(2) # 128 * 80 *80
-        
-        conv3_1 = nn.Conv2d(96, 128, 5, 1, 2) # 192 * 80 * 80
-        pool3 = nn.MaxPool2d(2) # 192 * 40 *40
-        
-        conv4_1 = nn.Conv2d(128, 128, 5, 1, 2) # 256 * 40 * 40
-        pool4 = nn.MaxPool2d(4) # 256 * 20 *20
-        
-        self.conv_module = nn.Sequential(
-            conv1_1,
-            conv1_2,
-            nn.LeakyReLU(),
-            pool1,
-            conv2_1,
-            conv2_2,
-            nn.LeakyReLU(),
-            pool2,
-            conv3_1,
-            nn.LeakyReLU(),
-            pool3,
-            conv4_1,
-            nn.LeakyReLU(),
-            pool4
-        )
-        fc1 = nn.Linear(128*10*10, 256)
-        fc2 = nn.Linear(256, 64)
-        fc3 = nn.Linear(64, 1)
-        
-        self.fc_module = nn.Sequential(
-            fc1,
-            nn.LeakyReLU(),
-            fc2,
-            nn.LeakyReLU(),
-            fc3,
-            nn.LeakyReLU()
-        )
-
-        # gpu로 할당
-
-    def forward(self, x):
-        out = self.conv_module(x) # @192*10*10
-        dim = 1
-        for d in out.size()[1:]: #192*10*10
-            dim = dim * d
-        out = out.view(-1, dim)
-        out = self.fc_module(out)
-        return out
 
 def next_batch(num, data, labels, i, batch_size):
     return np.asarray(data[i*batch_size:i*batch_size+batch_size]), np.asarray(labels[i*batch_size:i*batch_size+batch_size])
 
+###############
+'''
+Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun.
+Deep Residual Learning for Image Recognition.
+In CVPR, 2016.
+'''
+
+import torch.utils.model_zoo as model_zoo
+
+pretrained_settings = {
+    "cifar100": {
+        'resnet20': 'https://github.com/chenyaofo/CIFAR-pretrained-models/releases/download/resnet/cifar100-resnet56-2f147f26.pth',
+        'num_classes': 100
+    }
+}
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class CifarResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=10):
+        super(CifarResNet, self).__init__()
+        self.inplanes = 16
+        self.conv1 = conv3x3(3, 16)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def resnet_model():
+    model = CifarResNet(BasicBlock, [3, 3, 3], num_classes=100)
+    return model
+
+
+
+###############
 
 lossFunction = torch.nn.MSELoss() # TODO : change loss to mse 
 epochs = 500
 lr = 0.0001
 batch_size = 128
 display_step = 50
-img_shape = (3, 250, 250)
+img_shape = (3, 224, 224)
 # Initialize generator and discriminator
-regression = FRegression()
+regression = resnet_model()
+num_ftrs = regression.fc.in_features
+regression.fc = nn.Sequential(
+    nn.Linear(num_ftrs, 50),
+    nn.Linear(50, 1)
+)
 # Optimizers
 optimizer = torch.optim.Adam(regression.parameters(), lr=lr, weight_decay=lr/1000)
 Tensor = torch.FloatTensor
@@ -194,7 +261,7 @@ for epoch in range(epochs):
     random.shuffle(c)
     data_train, labels_train = zip(*c)
     epochLoss = 0.0
-    
+    regression.train()
     for i in range(0, (len(data_train)//batch_size)):  
         filename_batch, labels_batch = next_batch(batch_size, data_train, labels_train, i, batch_size)
         data_batch = make_image_batch(filename_batch)
@@ -209,7 +276,7 @@ for epoch in range(epochs):
     if (epoch+1) % display_step == 0:
     	print("epoch: %d, loss: %f" % (epoch, epochLoss/len(data_train)))
     train_losses.append(epochLoss/len(data_train))
-    
+    regression.eval()
     valid_loss = 0.0
     valid_num = 0
     for j in range(0, (len(data_val)//batch_size)):  
@@ -236,8 +303,8 @@ for epoch in range(epochs):
       print(f'Stopping training on epoch {epoch:d}')
       break
 
-model2.load_state_dict(torch.load(os.path.join(MODEL_DIR, 'fMin'+str(fMin)+'fMax'+str(fMax)+'checkpoint.pt')))
-
+regression.load_state_dict(torch.load(os.path.join(MODEL_DIR, 'fMin'+str(fMin)+'fMax'+str(fMax)+'checkpoint.pt')))
+regression.eval()
 epochLoss = 0.0
 for i in range(0, len(data_test)//batch_size):   
     filename_batch, labels_batch = next_batch(batch_size, data_test, labels_test, i, batch_size)
